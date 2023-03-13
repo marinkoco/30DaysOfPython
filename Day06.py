bf0b0b236b0cb67504e38109dc61be9d24616f8f
marinkoco <marinkoco@gmail.com>
2023-03-13 17:58:28 +0100

    Started Day06 up to 'Changing Tuples to Lists'

---
+++ Day06.py
@@ -0,0 +1,41 @@
+# A tuple is a collection of different data types which is ordered and unchangeable (immutable). Tuples are written with round brackets, (). Once a tuple is created, we cannot change its values. We cannot use add, insert, remove methods in a tuple because it is not modifiable (mutable). Unlike list, tuple has few methods. Methods related to tuples:
+# 
+# tuple(): to create an empty tuple
+# count(): to count the number of a specified item in a tuple
+# index(): to find the index of a specified item in a tuple
+# operator: to join two or more tuples and to create a new tuple
+# Creating a Tuple
+# Empty tuple: Creating an empty tuple
+
+# syntax
+empty_tuple = ()
+# or using the tuple constructor
+empty_tuple = tuple()
+
+# Accessing Tuple Items
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[0]
+second_fruit = fruits[1]
+last_index =len(fruits) - 1
+last_fruit = fruits[last_index]
+
+# Negative indexing
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[-4]
+second_fruit = fruits[-3]
+last_fruit = fruits[-1]
+
+# Slicing tuples
+# Range of Positive Indexes
+fruits = ('banana', 'orange', 'mango', 'lemon')
+all_fruits = fruits[0:4]    # all items
+all_fruits= fruits[0:]      # all items
+orange_mango = fruits[1:3]  # doesn't include item at index 3
+orange_to_the_rest = fruits[1:]
+
+# Range of Negative Indexes
+fruits = ('banana', 'orange', 'mango', 'lemon')
+all_fruits = fruits[-4:]    # all items
+orange_mango = fruits[-3:-1]  # doesn't include item at index 3
+orange_to_the_rest = fruits[-3:]
+
