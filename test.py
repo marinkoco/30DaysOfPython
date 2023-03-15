d0d2295b455b2421fd87096ab25a7848a358a0eb
marinkoco <marinkoco@gmail.com>
2023-03-15 19:51:57 +0100

    testing on Day08.py

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-animals = [1,2,3,4,5]
-animals.sort(reverse=True)
-print(animals)
+person = {
+    'first_name':'Ivan',
+    'last_name':'Marinkovic',
+    'age':250,
+    'country':'Serbia',
+    'is_marred':True,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+        }
+}
+values = person.values()
+print(values)
