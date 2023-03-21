bcf8d17c4d95d29b70dda73216fdc51a43daf5bd
marinkoco <marinkoco@gmail.com>
2023-03-21 15:47:03 +0100

    tested if - els with int(input(''))

--- test.py
+++ test.py
@@ -1,14 +1,9 @@
-person = {
-    'first_name':'Ivan',
-    'last_name':'Marinkovic',
-    'age':250,
-    'country':'Serbia',
-    'is_marred':True,
-    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
-    'address':{
-        'street':'Space street',
-        'zipcode':'02210'
-        }
-}
-values = person.values()
-print(values)
+# Get user input using input(“Enter your age: ”). 
+# If user is 18 or older, give feedback: You are old enough to drive. 
+# If below 18 give feedback to wait for the missing amount of years.
+age = int(input('Enter your age: '))
+driving_age = 18
+if driving_age - age <= 0:
+    print('You are old enough to learn to drive.')
+else:
+    print ('You need', abs(age-driving_age), 'more years to learn to drive.')
