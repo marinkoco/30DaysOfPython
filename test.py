a8cd9a5d05ea59ea211b08be219bcb0c47322f24
marinkoco <marinkoco@gmail.com>
2023-03-22 10:55:42 +0100

    commit of Day10.py - Loops -

--- test.py
+++ test.py
@@ -1,9 +1,31 @@
-# Get user input using input(“Enter your age: ”). 
-# If user is 18 or older, give feedback: You are old enough to drive. 
-# If below 18 give feedback to wait for the missing amount of years.
-age = int(input('Enter your age: '))
-driving_age = 18
-if driving_age - age <= 0:
-    print('You are old enough to learn to drive.')
-else:
-    print ('You need', abs(age-driving_age), 'more years to learn to drive.')
+person = {
+    'first_name': 'Ivan',
+    'last_name': 'Marinkovic',
+    'age': 250,
+    'country': 'Serbia',
+    'is_marred': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02220'
+    }
+}
+for key, value in person['address'].items():
+    if key == 'street':
+        print(key + ':', value)
+
+
+for i in range(1,8):
+    print('#' * i)
+
+for i in range(1,8):
+    print('#' * 8)
+
+for i in range(0,11):
+    print(i, 'x', i, '=', i*i)
+
+# Use for loop to iterate from 0 to 100 and print the sum of all numbers.
+total = 0
+for i in range(101):
+    total = total + i
+print(total)
