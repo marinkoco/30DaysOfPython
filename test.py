35e010c8ece825fa3276f28ab5d23212c974d182
marinkoco <marinkoco@gmail.com>
2023-03-22 15:41:53 +0100

    commit Day11.py up to / Function Returning a Value - Part 2 /

--- test.py
+++ test.py
@@ -1,31 +1,17 @@
-person = {
-    'first_name': 'Ivan',
-    'last_name': 'Marinkovic',
-    'age': 250,
-    'country': 'Serbia',
-    'is_marred': True,
-    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
-    'address': {
-        'street': 'Space street',
-        'zipcode': '02220'
-    }
-}
-for key, value in person['address'].items():
-    if key == 'street':
-        print(key + ':', value)
+def my_name():
+    fname = str(input("Input First Name:"))
+    lname = str(input("Input Last Name:"))
+    full_name = fname + ' ' + lname
+    return full_name
+print(my_name())
 
+def square(x):
+    return x * x
+print(square(5))
 
-for i in range(1,8):
-    print('#' * i)
-
-for i in range(1,8):
-    print('#' * 8)
-
-for i in range(0,11):
-    print(i, 'x', i, '=', i*i)
-
-# Use for loop to iterate from 0 to 100 and print the sum of all numbers.
-total = 0
-for i in range(101):
-    total = total + i
-print(total)
+def sum_num(n):
+    total = 0
+    for i in range(n+1):
+        total += i
+    print(total)
+print(sum_num(10))
