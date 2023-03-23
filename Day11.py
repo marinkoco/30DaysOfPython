afbf16053cc8766e0cc4d675dc4f854f4767e88c
marinkoco <marinkoco@gmail.com>
2023-03-23 16:03:13 +0100

    commit 2nd part of Day11.py

--- Day11.py
+++ Day11.py
@@ -112,3 +112,98 @@ def add_two_numbers (num1, num2):
 print(add_two_numbers(num2 = 3, num1 = 2)) # Order does not matter
 
 # Function Returning a Value - Part 2
+# If we do not return a value with a function, then our function is returning None by default. 
+# To return a value with a function we use the keyword return followed by the variable we are returning. 
+# We can return any kind of data types from a function.
+
+# Returning a string:
+def print_name(firstname):
+    return firstname
+print_name('Ivan') # Ivan
+
+def print_full_name(firstname, lastname):
+    space = ' '
+    full_name = firstname  + space + lastname
+    return full_name
+print_full_name(firstname='Ivan', lastname='Marinkovic')
+
+# Returning a number:
+def add_two_numbers (num1, num2):
+    total = num1 + num2
+    return total
+print(add_two_numbers(2, 3))
+
+def calculate_age (current_year, birth_year):
+    age = current_year - birth_year
+    return age;
+print('Age: ', calculate_age(2019, 1819))
+
+# Returning a boolean:
+def is_even (n):
+    if n % 2 == 0:
+        print('even')
+        return True    # return stops further execution of the function, similar to break 
+    return False
+print(is_even(10)) # True
+print(is_even(7)) # False
+
+# Returning a list:
+def find_even_numbers(n):
+    evens = []
+    for i in range(n + 1):
+        if i % 2 == 0:
+            evens.append(i)
+    return evens
+print(find_even_numbers(10))
+
+# Function with Default Parameters
+# Sometimes we pass default values to parameters, when we invoke the function. 
+# If we do not pass arguments when calling the function, their default values will be used.
+def greetings (name = 'Peter'):
+    message = name + ', welcome to Python for Everyone!'
+    return message
+print(greetings())
+print(greetings('Ivan'))
+
+def generate_full_name (first_name = 'Ivan', last_name = 'Marinkovic'):
+    space = ' '
+    full_name = first_name + space + last_name
+    return full_name
+
+print(generate_full_name())
+print(generate_full_name('David','Smith'))
+
+def calculate_age (birth_year,current_year = 2021):
+    age = current_year - birth_year
+    return age
+print('Age: ', calculate_age(1821))
+
+def weight_of_object (mass, gravity = 9.81):
+    weight = str(mass * gravity)+ ' N' # the value has to be changed to string first
+    return weight
+print('Weight of an object in Newtons: ', weight_of_object(100)) # 9.81 - average gravity on Earth's surface
+print('Weight of an object in Newtons: ', weight_of_object(100, 1.62)) # gravity on the surface of the Moon
+
+# Arbitrary Number of Arguments
+# If we do not know the number of arguments we pass to our function, we can create a function which can take arbitrary number of arguments by adding * before the parameter name.
+def sum_all_nums(*nums):
+    total = 0
+    for num in nums:
+        total += num     # same as total = total + num 
+    return total
+print(sum_all_nums(2, 3, 5)) # 10
+
+# Default and Arbitrary Number of Parameters in Functions
+def generate_groups (team,*args):
+    print(team)
+    for i in args:
+        print(i)
+print(generate_groups('Team-1','Ivan','Brook','David','Eyob'))
+
+# Function as a Parameter of Another Function
+# You can pass functions around as parameters
+def square_number (n):
+    return n * n
+def do_something(f, x):
+    return f(x)
+print(do_something(square_number, 3)) # 27
--- test.py
+++ test.py
@@ -1,17 +1,70 @@
-def my_name():
-    fname = str(input("Input First Name:"))
-    lname = str(input("Input Last Name:"))
-    full_name = fname + ' ' + lname
-    return full_name
-print(my_name())
+def find_even_numbers(n):
+    evens = []
+    for i in range(n + 1):
+        if i % 2 == 0:
+            evens.append(i)
+    return evens
+print(find_even_numbers(10))
 
-def square(x):
-    return x * x
-print(square(5))
-
-def sum_num(n):
+def suma_nums(*nums):
     total = 0
-    for i in range(n+1):
+    for i in nums:
         total += i
-    print(total)
-print(sum_num(10))
+    return total
+print(suma_nums(2,3,5))
+
+def generate_groups (team,*args):
+    print(team)
+    for i in args:
+        print(i)
+print(generate_groups('Team-1','Ivan','Brook','David','Eyob'))
+
+# Declare a function add_two_numbers. It takes two parameters and it returns a sum.
+def add_two_numbers(x, y):
+    return x+y
+print(add_two_numbers(4,3))
+
+# Write a function called add_all_nums which takes arbitrary number of arguments and sums all the arguments. 
+# Check if all the list items are number types. 
+# If not do give a reasonable feedback.
+def add_all_nums(*args):
+    if all(isinstance(arg,(int,float)) for arg in args):
+        return sum(args)
+    else:
+        return 'Error: All arguments should be numeric'
+print(add_all_nums(2,3,4,5))
+
+# Write a function called check-season, it takes a month parameter and returns the season: Autumn, Winter, Spring or Summer.
+def check_season(month):
+    Autumn = ['september', 'october', 'november']
+    Summer = ['jun', 'july', 'august']
+    Winter = ['december', 'january', 'february']
+    Spring = ['march', 'april', 'may']
+    if month in Autumn:
+        print(month, 'belongs to', 'Autumn')
+    elif month in Summer:
+        print(month, 'belongs to', 'Summer')
+    elif month in Winter:
+        print((month, 'belongs to', 'Winter'))
+    else:
+        print((month, 'belongs to', 'Spring'))
+check_season('july')
+
+# Declare a function named print_list. It takes a list as a parameter and it prints out each element of the list.
+def print_list(list):
+    list=['i','v','a','n']
+    for l in list:
+        print(l)
+print_list(list)  
+
+# Declare a function named reverse_list. It takes an array as a parameter and it returns the reverse of the array (use loops).
+def reverse_list(arr):
+    reversed_arr = []
+    for i in range(len(arr)-1, -1, -1):
+        reversed_arr.append(arr[i])
+    return reversed_arr
+arr=[1,2,3,5,6]
+reverse_list(arr)
+
+
+
