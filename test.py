46098a799ce0c06974f6b14a45b045baf23c04fa
marinkoco <marinkoco@gmail.com>
2023-03-24 15:33:57 +0100

    test on Day12.py

--- test.py
+++ test.py
@@ -1,71 +1,26 @@
-def find_even_numbers(n):
-    evens = []
-    for i in range(n + 1):
-        if i % 2 == 0:
-            evens.append(i)
-    return evens
-print(find_even_numbers(10))
+import os
+# Creating a directory
+os.mkdir('directory_name')
+# Changing the current directory
+os.chdir('path')
+# Getting current working directory
+os.getcwd()
+# Removing directory
+os.rmdir()
 
-def suma_nums(*nums):
-    total = 0
-    for i in nums:
-        total += i
-    return total
-print(suma_nums(2,3,5))
+# Here's an example of how to create a new directory in a specific path using Python's os module:
+import os
 
-def generate_groups (team,*args):
-    print(team)
-    for i in args:
-        print(i)
-print(generate_groups('Team-1','Ivan','Brook','David','Eyob'))
+# specify the path where you want to create a new directory
+path = "C:/temp1" # make sure you use FWD-SLASH '/'
 
-# Declare a function add_two_numbers. It takes two parameters and it returns a sum.
-def add_two_numbers(x, y):
-    return x+y
-print(add_two_numbers(4,3))
+# specify the name of the new directory you want to create
+dir_name = "PyTEST"
 
-# Write a function called add_all_nums which takes arbitrary number of arguments and sums all the arguments. 
-# Check if all the list items are number types. 
-# If not do give a reasonable feedback.
-def add_all_nums(*args):
-    if all(isinstance(arg,(int,float)) for arg in args):
-        return sum(args)
-    else:
-        return 'Error: All arguments should be numeric'
-print(add_all_nums(2,3,4,5))
+# combine the path and the directory name using os.path.join()
+new_dir = os.path.join(path, dir_name)
 
-# Write a function called check-season, it takes a month parameter and returns the season: Autumn, Winter, Spring or Summer.
-def check_season(month):
-    Autumn = ['september', 'october', 'november']
-    Summer = ['jun', 'july', 'august']
-    Winter = ['december', 'january', 'february']
-    Spring = ['march', 'april', 'may']
-    if month in Autumn:
-        print(month, 'belongs to', 'Autumn')
-    elif month in Summer:
-        print(month, 'belongs to', 'Summer')
-    elif month in Winter:
-        print((month, 'belongs to', 'Winter'))
-    else:
-        print((month, 'belongs to', 'Spring'))
-check_season('july')
-
-# Declare a function named print_list. It takes a list as a parameter and it prints out each element of the list.
-def print_list(list):
-    list=['i','v','a','n']
-    for l in list:
-        print(l)
-print_list(list)  
-
-# Declare a function named reverse_list. It takes an array as a parameter and it returns the reverse of the array (use loops).
-def reverse_list(arr):
-    reversed_arr = []
-    for i in range(len(arr)-1, -1, -1):
-        reversed_arr.append(arr[i])
-    return reversed_arr
-arr=[1,2,3,5,6]
-reverse_list(arr)
-
-#test change
+# create the new directory using os.mkdir()
+os.mkdir(new_dir)
 
 
