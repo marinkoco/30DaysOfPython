e1028c241d50b0512832f59795248300c89cdeab
marinkoco <marinkoco@gmail.com>
2023-03-27 10:30:43 +0200

    testing Day13.py

--- test.py
+++ test.py
@@ -1,26 +1,3 @@
-import os
-# Creating a directory
-os.mkdir('directory_name')
-# Changing the current directory
-os.chdir('path')
-# Getting current working directory
-os.getcwd()
-# Removing directory
-os.rmdir()
-
-# Here's an example of how to create a new directory in a specific path using Python's os module:
-import os
-
-# specify the path where you want to create a new directory
-path = "C:/temp1" # make sure you use FWD-SLASH '/'
-
-# specify the name of the new directory you want to create
-dir_name = "PyTEST"
-
-# combine the path and the directory name using os.path.join()
-new_dir = os.path.join(path, dir_name)
-
-# create the new directory using os.mkdir()
-os.mkdir(new_dir)
-
-
+numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
+neg_zero = [i for i in (numbers) if i<=0]
+print(neg_zero)
