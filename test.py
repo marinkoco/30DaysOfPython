b4443fda4869f18a5c173ae7ea41981afbe03084
marinkoco <marinkoco@gmail.com>
2023-05-04 15:51:50 +0200

    testing API

--- test.py
+++ test.py
@@ -1,4 +1,24 @@
-with open('C:/TEST/test.txt', 'w') as f:
-    f.write('Prva linija')
-with open('C:/TEST/test.txt', 'a') as f:
-    f.write('Druga linija.')
+# let's import the flask
+from flask import Flask, Response
+import json
+import pymongo
+import os
+
+app = Flask(__name__)
+
+MONGODB_URI = 'mongodb+srv://marinkoco:6t5g7qr6ewQFxRTV@30daysofpython.wzhhpfw.mongodb.net/?retryWrites=true&w=majority'
+client = pymongo.MongoClient(MONGODB_URI)
+db = client['30DaysofPython']  # accessing the database
+students = list(db.students.find())
+
+@app.route('/api/v1.0/students', methods=['GET'])
+def get_students():
+    response = Response(json.dumps(students), mimetype='application/json')
+    client.close()  # close the database connection
+    return response
+
+if __name__ == '__main__':
+    # for deployment
+    # to make it work for both production and development
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
